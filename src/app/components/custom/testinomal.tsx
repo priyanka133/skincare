d591359c825266ca7196d317de6de68697182380
priYA <piyapardeshi111@gmail.com>
2024-12-02 10:31:37 +0530

    slider

--- src/app/components/custom/testinomal.tsx
+++ src/app/components/custom/testinomal.tsx
@@ -3,8 +3,8 @@
 import {  FaQuoteRight } from "react-icons/fa";
 import { AiFillStar } from "react-icons/ai";
 import dynamic from "next/dynamic";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Slider = dynamic(() => import("react-slick"), { ssr: false });
 
